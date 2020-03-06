9fcf304e37e26580630a2667c1dcbe86c7294835
RuoyanMeng <ruoyanmeng@gmail.com>
2020-03-07 01:53:48 +0200

    test commit

--- src/Components/LyricsPic.js
+++ src/Components/LyricsPic.js
@@ -40,6 +40,16 @@ class LyricsPic extends Component {
         el.target.href = objurl;
     };
 
+    
+    dataURLtoBlob= (dataurl)=> {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], {type:mime});
+    }
+
     goBack(e) {
         e.preventDefault();
         this.props.history.goBack();
